665af62590c3ab11a0aaeb9ed6614b7d544fc6f8
James Weah <jweah@aggies.ncat.edu>
2023-11-22 09:49:23 -0500

    style: second portion of home page non responsive done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,54 @@
 import logo from '../Images/titlePicture.jpg'
+import calvinShoes from '../Images/HompagePhotos/calvin-shoes.jpg';
+import womenInTee from '../Images/HompagePhotos/women-in-brown.jpg';
+import womenWithGlasses from '../Images/HompagePhotos/women-with-glasses.jpg';
+
 import './Home.css'
 
 function Home() {
   return (
     <>
       <div>
-        <div>
-          <img className="first-page" src={logo} />
+        <section>
+          <img className="first-page-image" src={logo} />
+        </section>
+        <section>
+
+    <section className="second-page">
+      <div className="clothing">
+        <div className="clothes-collection">
+          <h1 className="clothes-sec">Collection</h1>
+          <h1 className="clothes-sec1">2023</h1>
+          <p className="clothes-sec2">SHOP NOW</p>
         </div>
-        
+        <img
+          className="women-in-brown"
+          src={womenInTee}
+          alt="girl-in-red"
+        />
+      </div>
+       <div className="asr-portion">
+        <img
+          className="girl-with-glasses"
+          src={womenWithGlasses}
+          alt="girl-with-glasses"
+        />
+        <div className="">
+          <h1 className="accessories">Accessories</h1>
+          <p className="shop-now">SHOP NOW</p>
+        </div>
+      </div>
+      <div className="shoes-portion">
+        <div className="shoes-text">
+          <h1 className="shoes-spring">Shoes Spring</h1>
+          <h1 className="shoes-date">2023</h1>
+          <p className="shoes-shop">SHOP NOW</p>
+        </div>
+        <img className="shoes-img" src={calvinShoes} alt="" />
+      </div> 
+    </section>
+        </section>
+        <section><h1>Hello</h1></section>
       </div>
     </>
   );
